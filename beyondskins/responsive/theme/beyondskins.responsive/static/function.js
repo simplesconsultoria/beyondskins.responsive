01083ea5754cf0135a3b54e5951fefa164e9ed42
Tamosauskas <thiago@simplesconsultoria.com.br>
2011-11-14 15:14:48 -0200

    Busca com overlay e correção na ordem dos scripts

--- beyondskins/responsive/theme/beyondskins.responsive/static/function.js
+++ beyondskins/responsive/theme/beyondskins.responsive/static/function.js
@@ -1,3 +1,22 @@
-jq(document).ready(function(jq) {
+jQuery(document).ready(function() {
     
+jQuery(document).ready(function(jq) {
+
+var buscalink = document.location.href + '/search'; 
+
+jQuery("#searchbt a").attr({
+          href:buscalink
+        });
+
+}); 
+	 
+jQuery('#searchbt a').prepOverlay({
+                subtype: 'ajax',
+                filter: '#content>*',
+                });
+
+
+				
+				
+
 });
